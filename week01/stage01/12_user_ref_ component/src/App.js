59716d723a6822f2dd1e62e899823962082ea4ba
KIMMOONKWAN <moon_kwan91@naver.com>
2022-02-24 11:15:23 +0900

    13 array add

--- week01/stage01/12_user_ref_ component/src/App.js
+++ week01/stage01/12_user_ref_ component/src/App.js
@@ -1,31 +1,72 @@
-import React, { useRef } from 'react';
-
+import React, { useRef, useState } from 'react';
 import UserList from './UserList';
+import CreateUser from './CreateUser';
 
 function App() {
-  const users = [
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: ''
+  });
+
+  const {username, email} = inputs;
+
+  const onChange = e => {
+    const {name,value} = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  };
+
+
+  const [users, setUsers] = useState([
     {
       id: 1,
-      username: 'neonex',
-      email: 'public.neonex@neonex.com'
+      username: 'velopert',
+      email: 'public.velopert@gmail.com'
     },
     {
       id: 2,
       username: 'tester',
-      email: 'tester@neonex.com'
+      email: 'tester@example.com'
     },
     {
       id: 3,
       username: 'liz',
-      email: 'liz@neonex.com'
+      email: 'liz@example.com'
     }
-  ];
+  ]);
 
   const nextId = useRef(4);
   const onCreate = () => {
+    
+    const user = {
+      id: nextId.current,
+      username,
+      email
+    };
+
+    setUsers([...users, user]);
+    //concat을 대신 써줘도 된다. oncat 함수는 기존의 배열을 수정하지 않고, 새로운 원소가 추가된 새로운 배열을 만들어줌
+    // setUsers(users.concat(user));
+    setInputs({
+      username: '',
+      email: ''
+    });
+
     nextId.current += 1;
   }
-  return <UserList users={users} />;
+  return (
+    <div>
+      <CreateUser 
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} />
+    </div>
+  ); 
 }
 
 export default App;
